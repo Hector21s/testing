925d8db149b5ee8c32936caf3c72a60b67e8fe4e
Héctor21s <hectoremmanuelmayahuareyes@gmail.com>
2023-10-23 16:36:46 -0600

    correlacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaComponent } from './media/media/media.component';
 import { RegresionLinealComponent } from './regresion-lineal/regresion-lineal.component';
+import { CorrelacionComponent } from './correlacion/correlacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediaComponent,
-    RegresionLinealComponent
+    RegresionLinealComponent,
+    CorrelacionComponent
   ],
   imports: [
     BrowserModule
